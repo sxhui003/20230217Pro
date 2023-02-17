89ae9e143605b5af1ffd66a6885746190ea340a9
sxhui003 <sxhui003@126.com>
2023-02-17 14:15:32 +0800

    second pull

--- src/com/atguigu/git/GitTest.java
+++ src/com/atguigu/git/GitTest.java
@@ -16,6 +16,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("push test");
+        System.out.println("push test2");
     }
 
 
