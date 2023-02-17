7f7e2c233ea674929aa6b47f51f149d7d41011ff
sonic <sxhui003@126.com>
2023-02-17 13:54:28 +0800

    third commit

--- src/com/atguigu/git/GitTest.java
+++ src/com/atguigu/git/GitTest.java
@@ -11,9 +11,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
     }
 
 
