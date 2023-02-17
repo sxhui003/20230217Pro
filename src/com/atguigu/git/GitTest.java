01e406b443d2c024916d1655a4b58144a6ab4f3b
sonic <sxhui003@126.com>
2023-02-17 13:52:41 +0800

    first commit

---
+++ src/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @aythor sonic
+ * @versison 1.0
+ */
+
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+
+}
