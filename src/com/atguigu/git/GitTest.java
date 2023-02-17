cc74d7fed63edb01e1c648fc62a6e26fce38dd44
sonic <sxhui003@126.com>
2023-02-17 13:58:35 +0800

    hox-fix first commit

--- src/com/atguigu/git/GitTest.java
+++ src/com/atguigu/git/GitTest.java
@@ -11,6 +11,9 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 
 
